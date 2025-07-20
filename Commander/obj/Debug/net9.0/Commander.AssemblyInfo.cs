4b8e5a973b47b14a2789288061468d6a50e2e13e
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2025-07-20 12:26:32 +0300

    frontend implemented

--- Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cca48e916b5d047c41fb2f1f607db2729ff3dc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b647e26e4380b6fab30eab82a6a82f1baa16ff0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Frontend/Pages/Login/Login.razor.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Frontend.Pages.Login
+{
+    public partial class Login : ComponentBase
+    {
+        [Inject]
+        public Frontend.Data.InterfaceContext DbContext { get; set; }
+
+        protected string username = string.Empty;
+        protected string password = string.Empty;
+        protected string? message;
+
+        protected async Task HandleLogin()
+        {
+            var user = await DbContext.Users
+                .FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+
+            message = user != null ? "تم تسجيل الدخول بنجاح" : "بيانات الدخول غير صحيحة";
+        }
+    }
+}
