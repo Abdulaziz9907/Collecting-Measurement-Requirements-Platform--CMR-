4f21a875174571835ad6885c16689b7e17cbdc66
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2025-07-27 18:11:34 +0300

    Remove initial EF Core migration files
    Deleted the initial Entity Framework Core migration and model snapshot files from the Migrations directory. This may be in preparation for a migration reset or restructuring of the database schema.

--- Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e03304b78cd3c76e2df085a35fa59b76b3762de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145eaa34b870e178a9d60459f21e84ca84742c04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
