8509c938172dd2afe0143130790e1e98e80a529d
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2025-08-11 11:02:59 +0300

    removed unnecessary files

--- Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6572e7fe8d978529416e3061a7c486e608ab349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343333ddaad22dfaca3f66b21eda499374a01e47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
