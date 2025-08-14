9ec36a653caef2b387a3542b8b7c1b21f6e70eb7
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2025-08-14 19:32:17 +0300

    favicon+title change

--- Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07bc0f726d179580ff7b440c53d16e75f7986e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f055cff2f5d534dd990f04740dd0e427728f617e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
