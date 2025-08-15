4598ed9afa04ef0656b2c0240f2a805b1e1c3103
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2025-08-15 15:15:28 +0300

    .

--- Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fca7a52acfe3d53e5e85a95e41c11d7cdd82d1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c687d56c53467a5a707fe66501314aadfbece0aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
