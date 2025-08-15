465868be0c5f57139d69313949b3e331011235e7
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2025-08-15 16:48:58 +0300

    deployment changes

--- Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
+++ Commander/obj/Debug/net9.0/Commander.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Commander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30eac5b05829fb0fb6f017a5ff1938dbac79a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62623a638a9b7aa0995d3c3b9209627a2ad335c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Commander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Commander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
